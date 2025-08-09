965c14215cf2e9c231236bb54e0b86fb86a7f033
grehov084 <grehov084@gmail.com>
2025-08-10 01:27:16 +0700

    123

--- assets/main.js
+++ assets/main.js
@@ -136,6 +136,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
         
     });
     siteOverlay.addEventListener("click", ()=>{
+        if(searchContent.classList.contains("show") && !mobileMenu.classList.contains("show")){
+            searchContent.classList.remove("show");
+            searchToggle.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z" stroke="#2B2A29" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M15 15L21 21" stroke="#2B2A29" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+            body.removeAttribute("style");
+            fadeOut(siteOverlay, 500);
+        }
         if(mobileMenu.classList.contains("show")){
             if(scrollVal != 0){
                 closeMobileMenu();
@@ -147,6 +153,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 closeMobileMenu();;
             }
         }
+
     });
 });
 
